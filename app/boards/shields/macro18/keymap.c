8b6e5fbb019aae7559b24927f1b5d9a120f92597
Opids14 <pidoguinto@gmail.com>
2025-12-06 15:02:25 -0800

    test

--- app/boards/shields/macro18/keymap.c
+++
@@ -1,9 +0,0 @@
-#include <zmk/keymap.h>
-
-const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = KEYMAP(
-        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F,
-        KC_G, KC_H, KC_I, KC_J, KC_K, KC_L,
-        KC_M, KC_N, KC_O, KC_P, KC_Q, KC_R
-    )
-};
